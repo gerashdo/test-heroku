e283d2f24e73076e4306405328470fb4dcab7e09
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2021-09-29 11:23:44 -0500

    Actualizado settings

--- reito/settings.py
+++ reito/settings.py
@@ -89,7 +89,8 @@ WSGI_APPLICATION = 'reito.wsgi.application'
 
 import dj_database_url
 from decouple import config
-DATABASE = {
+
+DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
