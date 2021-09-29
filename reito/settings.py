1e136a705cdd9d6d7e34b7dd3d1ce8a8c570b932
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2021-09-29 11:21:06 -0500

    Actualizado settings

--- reito/settings.py
+++ reito/settings.py
@@ -133,9 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 LOGIN_URL = reverse_lazy('usuarios:login')
