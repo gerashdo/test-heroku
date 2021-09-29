03789e87b79deb772fd9d5864cb047c0505f1b9b
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2021-09-29 14:52:12 -0500

    Modificar settings

--- reito/settings.py
+++ reito/settings.py
@@ -133,7 +133,7 @@ django_heroku.settings(locals())
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
